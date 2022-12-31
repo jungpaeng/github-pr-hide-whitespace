90e38fbeb5594b59b0db86b4e49df3d949e0a803
jungpaeng <meis1541@naver.com>
2023-01-01 02:04:55 +0900

    fix: 깃허브 내에서 페이지 이동시 동작하지 않는 이슈

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-(function main() {
-  window.addEventListener('load', () => {
-    const url = new URL(location);
-    if (url.searchParams.get('w')) return;
+const GITHUB_PR_HREF = /github.com\/.*\/.*\/(pull\/.*\/(files|commits))|compare/;
 
-    url.searchParams.set("w", "1");
-    window.history.replaceState(null, null, url.toString());
-    location.reload();
+function hideWhitespaces() {
+  const url = new URL(location);
+  if (!url.href.match(GITHUB_PR_HREF)) return;
+  if (url.searchParams.get('w')) return;
+
+  url.searchParams.set("w", "1");
+  window.history.replaceState(null, null, url.toString());
+  location.reload();
+}
+
+function onLocationChange(callback) {
+  let prevURL = document.location.href;
+
+  new MutationObserver((mutations) => {
+    mutations.forEach(() => {
+      if (prevURL !== location.href) {
+        prevURL = location.href;
+        callback();
+      }
+    });
+  }).observe(
+    document.querySelector("body"),
+    { childList: true, subtree: true },
+  );
+}
+
+(function main() {
+  hideWhitespaces();
+  window.addEventListener('load', () => {
+    onLocationChange(hideWhitespaces);
   });
 })();
